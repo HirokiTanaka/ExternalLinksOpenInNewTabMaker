cb4c84bb1d3dd7e83795db878dbb4b37dd51f233
tanakahiroki <tnkhrk.122@gmail.com>
2015-05-17 11:26:46 +0900

    modify ページロード後に作成されたリンクも対象に含めるように修正

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 (function() {
-  chrome.extension.sendRequest({}, function(response) {
-    if (response.isActive) {
-      $(document).ready(function() {
-        $("a[href^='http']:not([href*='" + location.hostname + "'])").attr('target', '_blank');
+  $(document).ready(function() {
+    chrome.extension.sendRequest({}, function(response) {
+      if (!response.isActive) return;
+      $(document).on('click', "a[href^='http']:not([href*='" + location.hostname + "'])", function() {
+        $this = $(this);
+        if ($this.attr('target')) return;
+        $this.attr('target', '_blank');
+        return true;
       });
-    }
+    });
   });
 })();
